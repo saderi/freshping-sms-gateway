d9a20fc4b864d711d12bb87839ce8f98fc71232f
payam <saderi@gmail.com>
2018-10-28 15:10:39 +0300

    logging webhook action in access.log file

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ $json_data = file_get_contents("php://input");
 if (empty($json_data))
     die();
 
+// Log webhook action
+$file = 'access.log';
+$current = file_get_contents($file);
+$current .= date('[j/M/Y H:i:s]'). " $json_data \n";
+file_put_contents($file, $current);
+
 // Get config variables
 require  'config.php';
 require  'vendor/autoload.php';
 
 $data = json_decode($json_data);
 $webhook_event_data = $data->webhook_event_data;
-
 if ($webhook_event_data->http_status_code != 200) {
 
     $message = "Your site has a problem. Site name: $webhook_event_data->application_name.";
-
     foreach ($activeSender as $senderName => $senderStatus) {
         if ($senderStatus) {
             require  "gateways/$senderName.php";
